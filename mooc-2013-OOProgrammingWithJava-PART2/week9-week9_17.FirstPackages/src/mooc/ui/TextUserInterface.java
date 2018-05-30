3051ad57a83e0039700e4e1e1e07498e9d857840
Meike <meike_hankewicz@yahoo.de>
2018-05-30 15:24:37 +0200

    exercise 17 First Packages

---
+++ mooc-2013-OOProgrammingWithJava-PART2/week9-week9_17.FirstPackages/src/mooc/ui/TextUserInterface.java
@@ -0,0 +1,10 @@
+package mooc.ui;
+
+import mooc.ui.UserInterface;
+
+public class TextUserInterface implements UserInterface{
+
+  public void update(){
+    System.out.println("Updating the user interface");
+  }
+}
