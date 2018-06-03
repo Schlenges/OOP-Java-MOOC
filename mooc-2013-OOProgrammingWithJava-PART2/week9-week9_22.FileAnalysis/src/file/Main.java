d20a18ea8d7b9684dcbbabb3d279206b56086f94
Meike <meike_hankewicz@yahoo.de>
2018-06-03 11:16:54 +0200

    deleted unnecessary class files

--- mooc-2013-OOProgrammingWithJava-PART2/week9-week9_22.FileAnalysis/src/file/Main.java
+++ mooc-2013-OOProgrammingWithJava-PART2/week9-week9_22.FileAnalysis/src/file/Main.java
@@ -1,14 +1,13 @@
 package file;
 
 import java.io.File;
-import file.*;
 
 public class Main {
 
     public static void main(String[] args) throws Exception {
         // test your code here
 
-        File file = new File("testfile.txt");
+        File file = new File("../testfile.txt");
         Analysis analysis = new Analysis(file);
         System.out.println("Lines: " + analysis.lines());
         System.out.println("Characters: " + analysis.characters());
