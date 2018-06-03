f2e102ca41f46c58cc1485f3395038af6b4b46cc
Meike <meike_hankewicz@yahoo.de>
2018-06-03 08:47:18 +0200

    refined solution exercise 21

--- mooc-2013-OOProgrammingWithJava-PART2/week9-week9_21.Printer/src/Printer.java
+++ mooc-2013-OOProgrammingWithJava-PART2/week9-week9_21.Printer/src/Printer.java
@@ -1,17 +1,30 @@
-import java.util.*;
-import java.io.*;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.io.File;
 
 public class Printer {
-
-  private File file;
   private Scanner reader;
+  private List<String> lines;
 
   public Printer(String fileName) throws Exception {
-      this.file = new File(fileName);
+      this.reader = new Scanner(new File(fileName));
+      this.lines = new ArrayList<String>();
+
+      while(reader.hasNextLine()){
+        lines.add(reader.nextLine());
+      }
   }
 
   public void printLinesWhichContain(String word) throws Exception {
-    this.reader = new Scanner(this.file);
+    for(String line : lines){
+      if(line.contains(word)){
+        System.out.println(line);
+      }
+    }
+
+
+/*  this.reader = new Scanner(this.file);
 
     while (reader.hasNextLine()) {
       String line = reader.nextLine();
@@ -21,7 +34,7 @@ public class Printer {
       }
     }
 
-    reader.close();
+    reader.close(); */
   }
 
 }
