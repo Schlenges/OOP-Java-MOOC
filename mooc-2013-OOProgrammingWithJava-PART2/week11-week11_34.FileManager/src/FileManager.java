2e229e6a2c6af258882cb6730f0705d6aaba5bd8
Meike <meike_hankewicz@yahoo.de>
2018-08-16 11:43:44 +0200

    exercise 34 FileManager

--- mooc-2013-OOProgrammingWithJava-PART2/week11-week11_34.FileManager/src/FileManager.java
+++ mooc-2013-OOProgrammingWithJava-PART2/week11-week11_34.FileManager/src/FileManager.java
@@ -10,12 +10,30 @@ import java.util.Scanner;
 public class FileManager {
 
     public List<String> read(String file) throws FileNotFoundException {
-        return null;
+        List<String> lines = new ArrayList<String>();
+        Scanner reader = new Scanner(new File(file));
+
+        while(reader.hasNextLine()){
+            lines.add(reader.nextLine());
+        }
+
+        return lines;
     }
 
     public void save(String file, String text) throws IOException {
+        FileWriter writer = new FileWriter(file);
+
+        writer.write(text);
+        writer.close();
     }
 
     public void save(String file, List<String> texts) throws IOException {
+        FileWriter writer = new FileWriter(file);
+
+        for(String text : texts){
+            writer.write(text + "\n");
+        }
+
+        writer.close();
     }
 }
--- mooc-2013-OOProgrammingWithJava-PART2/week11-week11_34.FileManager/src/Main.java
+++ mooc-2013-OOProgrammingWithJava-PART2/week11-week11_34.FileManager/src/Main.java
@@ -5,8 +5,13 @@ public class Main {
     public static void main(String[] args) throws IOException {
         FileManager t = new FileManager();
 
-        for (String line : t.read("src/testinput1.txt")) {
+        for (String line : t.read("testinput1.txt")) {
             System.out.println(line);
         }
+
+        t.save("test.txt", "Why hello there!");
+
+        t.save("test2.txt", t.read("testinput2.txt"));
+
     }
 }
