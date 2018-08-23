68f141edc5d67f3d05e03c7e1f26cf469a67f739
Meike <meike_hankewicz@yahoo.de>
2018-08-23 21:27:28 +0200

    exercise 35 Two Direction Dictionary

--- mooc-2013-OOProgrammingWithJava-PART2/week11-week11_35.TwoDirectionDictionary/src/dictionary/MindfulDictionary.java
+++ mooc-2013-OOProgrammingWithJava-PART2/week11-week11_35.TwoDirectionDictionary/src/dictionary/MindfulDictionary.java
@@ -1,28 +1,50 @@
 package dictionary;
 
 import java.util.*;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.FileNotFoundException;
+import java.io.IOException;
 
 public class MindfulDictionary{
-  private Map<String, String> dictionary = new HashMap<String, String>();
+  private Map<String, String> dictionary;
   private String fileName;
 
   public MindfulDictionary(){
-
+    dictionary = new HashMap<String, String>();
   }
 
   public MindfulDictionary(String file){
+    this();
     this.fileName = file;
   }
 
   public boolean load(){
-    File file = new File(this.fileName);
+    try{
+      Scanner reader = new Scanner(new File(fileName));
+
+      while (reader.hasNextLine()){
+        String line = reader.nextLine();
+        String[] words = line.split(":");
+        
+        add(words[0], words[1]);
+      }
+
+      reader.close();
+
+    } catch(FileNotFoundException e){
+        return false;
+      }
     
+    return true;
   }
 
   public void add(String word, String translation){
-    if(!(dictionary.containsKey(word))){
-      dictionary.put(word, translation);
+    if(dictionary.containsKey(word)){
+      return;
     }
+
+    dictionary.put(word, translation);
   }
 
   public String translate(String word){
@@ -61,6 +83,25 @@ public class MindfulDictionary{
 
       dictionary.remove(entry);
     }
+  }
+
+  public boolean save(){
+    try{
+      File file = new File(this.fileName);
+      FileWriter writer = new FileWriter(file);
+      String text = "";
+
+      for(String key : dictionary.keySet()){
+        text += key + ":" + dictionary.get(key) + "\n";
+      }
+
+      writer.write(text);
+      writer.close();
+    } catch(IOException e){
+      return false;
+    }
+
+    return true;    
   }
 
 }
