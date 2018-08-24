27bb48c9230b2cb9921e373f44dd264dd0667648
Meike <meike_hankewicz@yahoo.de>
2018-08-24 06:46:06 +0200

    exercise 37 Survey

--- mooc-2013-OOProgrammingWithJava-PART2/week11-week11_37.Survey/src/survey/UserInterface.java
+++ mooc-2013-OOProgrammingWithJava-PART2/week11-week11_37.Survey/src/survey/UserInterface.java
@@ -10,7 +10,35 @@ public class UserInterface implements Runnable {
 
     @Override
     public void run() {
-        // Create your app here
+        frame = new JFrame("Survey");
+        frame.setPreferredSize(new Dimension(200, 300));
+
+        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+
+        createComponents(frame.getContentPane());
+
+        frame.pack();
+        frame.setVisible(true);
+    }
+
+    private void createComponents(Container container){
+        BoxLayout layout = new BoxLayout(container, BoxLayout.Y_AXIS);
+        container.setLayout(layout);
+
+        container.add(new JLabel("Are you?"));
+        container.add(new JCheckBox("Yes!"));
+        container.add(new JCheckBox("No!"));
+
+        container.add(new JLabel("Why?"));
+        ButtonGroup why = new ButtonGroup();
+        JRadioButton noReason = new JRadioButton("No reason.");
+        JRadioButton fun = new JRadioButton("Because it is fun!");
+        why.add(noReason);
+        why.add(fun);
+        container.add(noReason);
+        container.add(fun);
+
+        container.add(new JButton("Done!"));
     }
 
 
