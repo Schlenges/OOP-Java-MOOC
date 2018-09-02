4f03f9d5fac84ab614b7347bdd5fe312969a5c43
Meike <meike_hankewicz@yahoo.de>
2018-09-02 11:00:11 +0200

    exercise 47.4

---
+++ mooc-2013-OOProgrammingWithJava-PART2/week12-week12_47.FlexibleFilteringCriteria/src/reader/criteria/Both.java
@@ -0,0 +1,17 @@
+package reader.criteria;
+
+public class Both implements Criterion{
+
+  Criterion one;
+  Criterion two;
+
+  public Both(Criterion one, Criterion two){
+    this.one = one;
+    this.two = two;
+  }
+
+  @Override
+  public boolean complies(String line){
+    return one.complies(line) && two.complies(line); 
+  }
+}
