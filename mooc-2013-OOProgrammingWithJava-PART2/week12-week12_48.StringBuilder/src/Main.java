88be8dd858e9aef6964a16914aafe962ee176dac
Meike <meike_hankewicz@yahoo.de>
2018-09-02 12:12:54 +0200

    exercise 48 String Builder

--- mooc-2013-OOProgrammingWithJava-PART2/week12-week12_48.StringBuilder/src/Main.java
+++ mooc-2013-OOProgrammingWithJava-PART2/week12-week12_48.StringBuilder/src/Main.java
@@ -1,3 +1,4 @@
+import java.lang.StringBuilder;
 
 public class Main {
 
@@ -7,6 +8,25 @@ public class Main {
     }
 
     public static String build(int[] t) {
-        return "{\n}\n";
+        StringBuilder sb = new StringBuilder();
+
+        sb.append("{");
+
+        for(int i = 0; i < t.length; i++){
+            if(i % 4 == 0){
+                sb.append("\n ");
+            }
+
+            sb.append(t[i]);
+
+            if(i != t.length-1){
+                sb.append(", ");
+            }
+            
+        }
+
+        sb.append("\n}");
+
+        return sb.toString();
     }
 }
