fd766b8e646653e288a8ef1b72ded13925811120
Meike <meike_hankewicz@yahoo.de>
2018-09-03 13:23:42 +0200

    exercise 49.7

--- mooc-2013-OOProgrammingWithJava-PART2/week12-week12_49.WormGame/src/wormgame/Main.java
+++ mooc-2013-OOProgrammingWithJava-PART2/week12-week12_49.WormGame/src/wormgame/Main.java
@@ -8,7 +8,20 @@ import wormgame.game.WormGame;
 public class Main {
 
     public static void main(String[] args) {
-        WormGame game = new WormGame(6, 3);
-        System.out.println(game.getApple());
+        WormGame game = new WormGame(20, 20);
+
+        UserInterface ui = new UserInterface(game, 20);
+        SwingUtilities.invokeLater(ui);
+
+        while (ui.getUpdatable() == null) {
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException ex) {
+                System.out.println("The drawing board hasn't been created yet.");
+            }
+        }
+
+        game.setUpdatable(ui.getUpdatable());
+        game.start();
     }
 }
